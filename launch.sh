5391c4e155b5fa11e2662f3c0a198393ded3e4f4
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-08-24 00:58:39 -0400

    feat: add support for the Miyoo A30, Miyoo Flip and Miyoo Mini Plus

--- launch.sh
+++ launch.sh
@@ -213,7 +213,7 @@ main() {
         export PLATFORM="tg5040"
     fi
 
-    allowed_platforms="tg5040 rg35xxplus"
+    allowed_platforms="miyoomini my282 my355 tg5040 rg35xxplus"
     if ! echo "$allowed_platforms" | grep -q "$PLATFORM"; then
         show_message "$PLATFORM is not a supported platform" 2
         return 1
